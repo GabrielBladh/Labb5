b14b1ee1e7b09b7cfbe67be5d200aed8fb8ca6ac
GabrielBladh <gabrielbladh@hotmail.com>
2025-09-18 14:56:04 +0200

    Skapat och kodat WordLength

---
+++ WordLength.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class WordLength
+{
+    public static void main(String[] Args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Skriv ett ord");
+        String ord = scan.nextLine();
+        int OrdLängd = ord.length();
+        System.out.printf("Ordet %s är %d långt !%n", ord, OrdLängd);
+    }
+}
+
