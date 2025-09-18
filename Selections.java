f320c3d2cbfd29c8c8b94a6cb3b8b049edb3bbd0
GabrielBladh <gabrielbladh@hotmail.com>
2025-09-18 17:20:27 +0200

    Några ändringar

--- Selections.java
+++ Selections.java
@@ -11,15 +11,15 @@ public class Selections
     {
         System.out.println("Heltalet är större än 67");
     }
-    if (Heltal == 3 || Heltal == 6 || Heltal == 9);
+    if (Heltal == 3 || Heltal == 6 || Heltal == 9)
     {
         System.out.println("Heltalet är antingen 3, 6 eller 9");
     }
-    if (Heltal >= 25 && Heltal <= 50);
+    if (Heltal >= 25 && Heltal <= 50)
     {
         System.out.println("Heltalet är mellan 25 och 50");
     }
-    if (Heltal >= 1 && Heltal <= 4 || Heltal >= 7 && Heltal <= 9);
+    if (Heltal >= 1 && Heltal <= 4 || Heltal >= 7 && Heltal <= 9)
     {
         System.out.println("Heltalet är mellan 1 och 4 eller mellan 7 och 9");
     }
