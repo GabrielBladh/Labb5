9285abfb04820547038bbe709394c978b3abff3c
GabrielBladh <gabrielbladh@hotmail.com>
2025-09-18 18:33:12 +0200

    Skapade filen Nestling

---
+++ Nestling.java
@@ -0,0 +1,23 @@
+public class Nestling {
+
+    int i = 0;
+    String s = "s";
+
+    if(i == 0 && s == "s" || s == "r")
+    {
+        System.out.println("0");
+    } 
+    else if(i == 1 && s == "s" || s == "r")
+    {
+        System.out.println("1");
+    }
+    else if(i == 2 && s == "s" || s == "r")
+    {
+        System.out.println("2");
+    }
+    else
+    {
+        System.out.println("3");
+    }
+
+}
